5608aec767fb2f2d6b95b59feab9e52d9846db69
Almujahid Jamion <94305038+firemelon96@users.noreply.github.com>
2025-05-17 19:05:08 +0800

    07: Video Categories

--- src/app/(home)/client.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { trpc } from '@/trpc/client';
-
-export const ClientPage = () => {
-  const [data] = trpc.hello.useSuspenseQuery({ text: 'estong dddf' });
-
-  return <div>Client page says: {data.greeting}</div>;
-};
--- src/app/(home)/protected/page.tsx
+++
@@ -1,5 +0,0 @@
-const ProtectedRoute = () => {
-  return <div>this is protected</div>;
-};
-
-export default ProtectedRoute;
