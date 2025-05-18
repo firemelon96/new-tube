7f7a9dfc9238a6f701f4d655b77680484b76ae59
Almujahid Jamion <94305038+firemelon96@users.noreply.github.com>
2025-05-19 01:55:11 +0800

    added infinite scroll functionality

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -1,15 +1,81 @@
 'use client';
+import { InfiniteScroll } from '@/components/infinite-scroll';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
 import { DEEFAULT_LIMIT } from '@/constants';
 import { trpc } from '@/trpc/client';
+import Link from 'next/link';
 import { Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 
 export const VideosSection = () => {
-  const data = trpc.studio.getMany.useSuspenseInfiniteQuery(
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <ErrorBoundary fallback={<p>Error...</p>}>
+        <VideosSectionSuspense />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
+
+const VideosSectionSuspense = () => {
+  const [videos, query] = trpc.studio.getMany.useSuspenseInfiniteQuery(
     {
       limit: DEEFAULT_LIMIT,
     },
     { getNextPageParam: (lastPage) => lastPage.nextCursor }
   );
-  return <div>{JSON.stringify(data)}</div>;
+  return (
+    <div>
+      <div className='border-y'>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className='pl-6 w-[510px]'>Video</TableHead>
+              <TableHead>Visibility</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className='text-righ'>Views </TableHead>
+              <TableHead className='text-righ'>Comments</TableHead>
+              <TableHead className='text-righ pr-6'>Likes</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {videos.pages
+              .flatMap((page) => page.items)
+              .map((video) => (
+                <Link
+                  href={`/studio/videos/${video.id}`}
+                  key={video.id}
+                  legacyBehavior
+                >
+                  <TableRow className='cursor-pointer'>
+                    <TableCell className='pl-6'>{video.title}</TableCell>
+                    <TableCell>visibility</TableCell>
+                    <TableCell>status</TableCell>
+                    <TableCell>date</TableCell>
+                    <TableCell>views</TableCell>
+                    <TableCell>comments</TableCell>
+                    <TableCell className='pr-6'>likes</TableCell>
+                  </TableRow>
+                </Link>
+              ))}
+          </TableBody>
+        </Table>
+      </div>
+      {/* {JSON.stringify(query)} */}
+      <InfiniteScroll
+        // isManual
+        hasNextPage={query.hasNextPage}
+        isFetchingNextPage={query.isFetchingNextPage}
+        fetchNextPage={query.fetchNextPage}
+      />
+    </div>
+  );
 };
