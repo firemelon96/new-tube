886c15c3e64194550b9c8e2559457fff1adaebd3
Almujahid Jamion <94305038+firemelon96@users.noreply.github.com>
2025-05-18 20:48:41 +0800

    08: Studio Layout

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const protectedRoute = createRouteMatcher(['/protected']);
+const protectedRoute = createRouteMatcher(['/studio(.*)']);
 
 export default clerkMiddleware(async (auth, req) => {
   if (protectedRoute(req)) await auth.protect();
---
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -0,0 +1,6 @@
+'use client';
+import { trpc } from '@/trpc/client';
+
+export const VideosSection = () => {
+  return <div>Videos section</div>;
+};
